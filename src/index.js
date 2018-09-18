22f77635038f2281a323a0405fd117e2483cbd23
Claiyre <mayingying1996@163.com>
2018-09-18 19:42:43 +0800

    add onAllUploaded

--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ const h = new Handlers()
  * @param params 参数对象
  * @param params.target required 一个File对象/File对象数组/type='file'的input
  * @param params.trunkSize 分片大小，最大为4M
+ * @param params.fileExts {Array} 可接受的文件类型，如['MP4', 'MKV']。 若不填，则表示不限制上传文件的类型
  * @param params.Nouce  随机数，用于接口鉴权
  * @param params.AppKey  开发者平台分配的AppKey，用于接口鉴权
  * @param params.CheckSum  服务器认证需要，SHA1(AppSecret+Nonce+CurTime)，16进制字符小写
- * @param params.[onError/onProgress/onAllUploaded/onUploaded/onAdd/existFn] 各种监听事件，初始化后可通过 instance.on('error', handler)添加
- * Nouce,AppKey,CheckSum用于接口鉴权，可在应用服务器端存储和使用，但不应存储或传递到客户端，也不应在网页等前端代码中嵌入。因此不建议传入这三个参数，而是通过中间层拦ajax请求，将鉴权信息添加到header中。若这三个参数都被传入了，则nim-loader将会自行添加接口鉴权信息
+ * @param params.[onError/onProgress/onUploaded] 各种监听事件，初始化后可通过 instance.on('error', handler)添加
+ * Nouce,AppKey,CheckSum用于接口鉴权，可在应用服务器端存储和使用，但不应存储或传递到客户端，也不应在网页等前端代码中嵌入。
+ * 因此不建议传入这三个参数，而是通过中间层拦ajax请求，将鉴权信息添加到header中。若这三个参数都被传入了，则nim-loader将会自行添加接口鉴权信息
  */
 class Uploader {
   constructor (params) {
     const options = Object.assign({}, defaultOptions, arguments)
     this.fileList = []
-    checkParams(params, h.onError)
+    checkParams.call(this)
     Object.keys(options).forEach(key => {
       if (key.startsWith('on')) {
         this.on(key, options[key])
@@ -60,6 +62,7 @@ class Uploader {
    */
   addFile (param) {
     let files = []
+    let exts = this.options.fileExts
     if (param instanceof File) {
       files.push(param)
     } else if (param instanceof Array && param.every(item => item instanceof File)) {
@@ -67,17 +70,34 @@ class Uploader {
     } else {
       h.onError(new Error(`Method addFile's param can only be File object or File object array, not ${param}`))
     }
-    files.forEach(item => this.fileList.push(item))
+    if (exts instanceof Array && exts.length > 0) {
+      files.forEach(item => this.fileList.push(new NimFile(item)))
+    } else {
+      /* 文件类型检查 */
+      files.forEach(item => {
+        let ext = item.name.split('.').pop()
+        if (exts.find(ext)) {
+          this.fileList.push(new NimFile(item))
+        } else {
+          h.onFileTypeNotMatch(ext)
+        }
+      })
+    }
   }
   /**
    * 上传所有文件
    */
   upLoadAll () {
+    let promiseArr = []
     for (let i = 0; i < this.fileList.length; i++) {
       if (this.fileList[i].checked) {
-        this.upLoadFile(i)
+        promiseArr.push(this.upLoadFile(i))
       }
     }
+    return Promise.all(promiseArr).then(res => {
+      h.onAllUploaded()
+      return Promise.resolve()
+    })
   }
   /**
    * 根据索引上传文件
@@ -105,13 +125,16 @@ class Uploader {
       // 获取偏移量
       offset = res.offset
       return this._uploadTrunk(file, xNosToken, bucket, object, offset, address)
+    }).then(res => {
+      h.onUploaded(file.fileKey)
+      return Promise.resolve(file.fileKey)
     }).catch(err => {
       h.onError(err)
     })
   }
   /**
    * 上传文件分片
-   * @param {FileList} file  // TODO 新的File类
+   * @param {FileList} file
    * @param {String} xNosToken 上传凭证
    * @param {String} bucket 存储上传文件的桶名
    * @param {String} object 存储上传文件的对象名
@@ -130,7 +153,7 @@ class Uploader {
       context: localStorage.getItem(file.fileKey + '_context') || ''
     })
     url = `${address}/${bucket}/${object}?${url}`
-    this._fly.post(url, trunkData, {
+    return this._fly.post(url, trunkData, {
       headers: {
         'x-nos-token': xNosToken,
         'Content-Length': trunkSize
@@ -140,19 +163,22 @@ class Uploader {
         return Promise.reject(new Error(`File upload request ${res.requestId} failed`))
       }
       localStorage.setItem(file.fileKey + '_context', res.context)
+      file.updateProgress(res.offset)
       if (res.offset < file.size) {
-        // 继续上传下一片
-        that._uploadTrunk(file, xNosToken, bucket, object, res.offset, address)
+        /* 继续上传下一片 */
+        return that._uploadTrunk(file, xNosToken, bucket, object, res.offset, address)
       } else {
-        // 更改状态
-        that.onUploaded(file.fileKey)
+        return Promise.resolve(res)
       }
     }).catch(err => {
       h.onError(err)
     })
   }
+  /**
+   * 移除文件
+   * @param {Number} fileIndex 文件索引
+   */
   removeFile (fileIndex) {
-    // TODO 删除文件并终止上传
     let file = this.fileList[fileIndex]
     this.fileList.splice(fileIndex, 1)
     file.ruin()
